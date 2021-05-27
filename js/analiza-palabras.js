96e9fea61e1c7ad591d555d16bbd0f6a9c401391
Xavier Escalante <xesgawork@gmail.com>
2021-05-27 18:36:08 +0200

    añadiendo js

--- js/analiza-palabras.js
+++ js/analiza-palabras.js
@@ -1,1 +1,44 @@
-/* Crea tu función aquí */
+function analizaPalabras(arrayPalabras) {
+  let informacionPalabra = "";
+
+  arrayPalabras.forEach((palabra, indice) => {
+    informacionPalabra += `#######\nPalabra: ${indice + 1}\n`;
+    informacionPalabra += `NÂº de caracteres: ${palabra.length}\n`;
+    informacionPalabra += `Deletreo: ${deletrearPalabra(palabra)}\n`;
+    informacionPalabra += checkParYMayus(palabra);
+    informacionPalabra += contieneBle(palabra);
+  });
+
+  informacionPalabra += "#######";
+
+  return informacionPalabra;
+}
+
+function deletrearPalabra(palabra) {
+  return palabra.split("").join("-");
+}
+
+function checkParYMayus(palabra) {
+  let resultado = "";
+  if (palabra.length % 2 === 0) {
+    resultado += "La palabra es par y ";
+  } else {
+    resultado += "La palabra es impar y ";
+  }
+
+  if (palabra.charAt(0) === palabra.charAt(0).toUpperCase()) {
+    resultado += "empieza por mayúscula\n";
+  } else {
+    resultado += "no empieza por mayúscula\n";
+  }
+
+  return resultado;
+}
+
+function contieneBle(palabra) {
+  if (palabra.includes("ble")) {
+    return 'La palabra sí contiene "ble"\n';
+  } else {
+    return 'La palabra no contiene "ble"\n';
+  }
+}
